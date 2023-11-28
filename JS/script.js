a2ff42823230a07fd3bde78647038a5de473e187
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-28 14:54:15 +0200

    css

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,31 @@
 // Write a JavaScript function to find the unique elements from two arrays.
-// callback function used to find the unique elements
-// function name taken from console test 
-function difference(array1, array2) {
-    //  used to convert both arrays into strings
-    let arrayOne = JSON.stringify(array1, true);
-    let arrayTwo = JSON.stringify(array2, true);
-    // filter() method is used to remove all the elements from array1 that are present in array
-    console.log(JSON.parse(arrayOne).filter(x => !JSON.parse(arrayTwo).includes(x)));
+function difference(arr1, arr2) {
+    const a1 = JSON.parse(arr1);
+    const a2 = JSON.parse(arr2);
 
-    // create a new array -  keep track of the elements that are present in the first array
-    let newArray = [];
-        // iterates over array1
-        for (let i = 0; i < array1.length; i++)
-        // the element in newArray is set to false
-        newArray[array1[i]] = false;
-        // iterates over array2
-        for (let i = 0; i < array2.length; i++)
-        // checks whether the value of newArray[array2[i]] is true. If it is, then the code inside the if block will be executed.
-        if (newArray[array2[i]] === true)
-    // create a new array - used to store the elements that are present in the first array but not in the second array
-    let storageArray = [];
+    const a = [];
+    const diff = [];
+    for (let i = 0; i < a1.length; i++)
+        a[a1[i]] = false;
+    for (let i = 0; i < a2.length; i++)
+        if (a[a2[i]] === true)
+            { a.splice(a.indexOf(a2[i]), 1); }
+        else a[a2[i]] = true;
+    for (const k in a)
+        diff.push(k);
+    return diff;
 }
-console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+
+// Add an event listener for the button click
+document.getElementById('calculateButton').addEventListener('click', function() {
+    const array1 = document.getElementById('array1').value;
+    const array2 = document.getElementById('array2').value;
+
+    const result = difference(array1, array2);
+
+    // Display result in HTML
+    document.getElementById('result').innerText = 'Difference: ' + JSON.stringify(result);
+
+    // Display result in the console
+    console.log('Difference:', result);
+});
